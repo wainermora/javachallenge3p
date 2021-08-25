4365865a315964879fd513b9b330c891bcec84f8
Wainer Mora <wainermora@gmail.com>
2021-08-24 22:09:05 -0600

    Project generated

---
+++ src/test/java/com/challlenge/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challlenge.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
